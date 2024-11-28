1c22108b37db35d6909f6da2cff871531ccc142d
Vanshaj <vanshajgupta37@gmail.com>
2024-11-28 20:49:02 +0530

    updated changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import Layout from './components/shared/Layout'
 import RegisterForm from './components/auth/RegisterForm'
 import LoginForm from './components/auth/LoginForm'
+import AdminDashboard from './components/admin/AdminDashboard'
 function App() {
   return (
     <Router>
       <LoginForm />
       <RegisterForm />
       <Layout />
+      <AdminDashboard />
     </Router>
   )
 }
