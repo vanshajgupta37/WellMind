4bc6ec7c4f31f46bb1c472645d9b8c39fcdd5ec9
Shivanshuk28 <2contactshivanshu@gmail.com>
2024-11-28 20:44:53 +0530

    admindashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,15 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import Layout from './components/shared/Layout'
 import RegisterForm from './components/auth/RegisterForm'
 import LoginForm from './components/auth/LoginForm'
+import AdminDashboard from './components/admin/AdminDashboard'
+// import TherapistBrowse from './components/client/TherapistSearch'
+
+
 function App() {
   return (
     <Router>
+    <AdminDashboard/>
+    {/* <TherapistBrowse/> */}
       <LoginForm />
       <RegisterForm />
       <Layout />
