81be1b93140406ab73cd7365ab9f7481f1afac0f
Vanshaj <vanshajgupta37@gmail.com>
2024-12-05 23:21:23 +0530

    PR-6 Minor Frontend Changes

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -123,6 +123,7 @@ const Appointment = () => {
           {
             
             Slots.length && Slots.map((item, index)=>(
+              item.length > 0 && 
               <div onClick={() => setSlotIndex(index)} className={`text-center py-6 min-w-16 rounded-full cursor-pointer ${slotIndex === index ? 'bg-primary text-white' : 'border border-gray-200'}`} key={index}>
                 <p>{item[0] && daysOfWeek[item[0].datetime.getDay()]}</p>
                 <p>{item[0] && item[0].datetime.getDate()}</p>
@@ -132,11 +133,20 @@ const Appointment = () => {
         </div>
 
         <div className='flex items-center gap-3 w-full overflow-x-scroll mt-4' >
-          {Slots.length && Slots[slotIndex].map((item, index) => (
+          {Slots.length>0 && Slots[slotIndex].length>0 ? (
+            Slots[slotIndex].map((item, index) => (
             <p onClick={()=> setSlotTime(item.time)} className={`text-sm font-light flex-shrink-0 px-5 py-2 rounded-full cursor-pointer ${item.time === slotTime ? 'bg-primary text-white' : 'text-gray-400 border-gray-300'}`} key = {index}>
               {item.time.toLowerCase()}
             </p>
-          ))}
+          ))
+        )
+        :
+        (
+          <div>
+          <p className='text-gray-400'>No slots Today!</p>
+          <p className='text-gray-400'>Book for upcoming days :)</p>
+          </div>
+        )}
         </div>
         <button className='bg-primary text-white text-sm font-light px-14 py-3 rounded-full my-6'>Book an Appointment</button>
       </div>
