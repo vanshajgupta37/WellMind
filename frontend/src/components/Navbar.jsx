618ad099f479d4cc48f33d0f0057caecaebc2b9a
Shivanshuk28 <2contactshivanshu@gmail.com>
2024-12-27 13:22:51 +0530

    bug fixed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -72,7 +72,14 @@ const Navbar = () => {
         {/* ---- Mobile View ---- */}
         <div className={` ${showMenu? 'fixed w-full' : 'h-0 w-0'} md:hidden right-0 top-0 botttom-0 z-20 overflow-hidden bg-white transition-all`} >
           <div className="flex items-center justify-between px-5 py-6" >
-            <img className="w-36" src={assets.logo} alt="" />
+          <div className="flex-shrink-0  flex items-center">
+                <Brain onClick={()=>{
+        navigate('/')
+      }} className="h-6 w-6 cursor-pointer text-teal-600" />
+                <span onClick={()=>{
+        navigate('/')
+      }} className="ml-2 text-xl  cursor-pointer font-semibold text-teal-600">WellMind</span>
+          </div>
             <img className="w-7" onClick={()=>setShowMenu(false)} src={assets.cross_icon} alt="" />
           </div>
           <ul className="flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium" >
